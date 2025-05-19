d294da90e368a2629efac236183c34f5c2ffca69
danirodriguezdj <150760368+danirodriguezdj@users.noreply.github.com>
2025-05-19 02:40:41 +0200

    Add files via upload

--- hippyrun/firebase.js
+++ hippyrun/firebase.js
@@ -1,20 +1,13 @@
-// Configuración e integración con Firebase
 
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDvRbMWv4TUwDPZGXsCWDUxkD7cPcw9v98",
   authDomain: "hippyrun.firebaseapp.com",
   projectId: "hippyrun",
-  storageBucket: "hippyrun.firebasestorage.app",
+  storageBucket: "hippyrun.appspot.com",
   messagingSenderId: "1006542760096",
-  appId: "1:1006542760096:web:6f9616ca50c7de917a81c5",
-  databaseURL: "https://hippyrun-default-rtdb.firebaseio.com/"
+  appId: "1:1006542760096:web:6f9616ca50c7de917a81c5"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db, ref, set, get, child };
